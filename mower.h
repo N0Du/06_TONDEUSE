18fa755e01609a2cebbcdad2d7a6d48141ab14e9
noamd <noam.dugerdil@bluewin.ch>
2022-11-22 19:03:22 +0100

    updated names

---
+++ mower.h
@@ -0,0 +1,21 @@
+// Fichier     nom du fichier (.h / .cpp)
+// Auteur      Noam Dugerdil
+// Date        ...
+//
+// But         description détaillée du projet
+//
+// Remarque
+//
+// Modifs      Date / Auteur / Raison
+//
+// Compilateur Bundled MinGW - Version w64 9.0
+
+#ifndef LABO_TONDEUSE_TONDEUSE_H
+#define LABO_TONDEUSE_TONDEUSE_H
+
+using Field = vector<vector<char>>;
+
+int DisplayField(Field field);
+
+
+#endif //LABO_TONDEUSE_TONDEUSE_H
---
+++ utils.h
@@ -0,0 +1,20 @@
+// Fichier     nom du fichier (.h / .cpp)
+// Auteur      Noam Dugerdil
+// Date        ...
+//
+// But         description détaillée du projet
+//
+// Remarque
+//
+// Modifs      Date / Auteur / Raison
+//
+// Compilateur Bundled MinGW - Version w64 9.0
+
+#ifndef LABO_TONDEUSE_ANNEXE_H
+#define LABO_TONDEUSE_ANNEXE_H
+
+
+int RandomInt(int min, int max);
+
+
+#endif //LABO_TONDEUSE_ANNEXE_H
