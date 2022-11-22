c9361659e57134026f84fae30b8b85a3924e5a23
noamd <noam.dugerdil@bluewin.ch>
2022-11-22 18:25:32 +0100

    initial

---
+++ tondeuse.h
@@ -0,0 +1,16 @@
+// Fichier     nom du fichier (.h / .cpp)
+// Auteur      Noam Dugerdil
+// Date        ...
+//
+// But         description détaillée du projet
+//
+// Remarque
+//
+// Modifs      Date / Auteur / Raison
+//
+// Compilateur Bundled MinGW - Version w64 9.0
+
+#ifndef LABO_TONDEUSE_TONDEUSE_H
+#define LABO_TONDEUSE_TONDEUSE_H
+
+#endif //LABO_TONDEUSE_TONDEUSE_H
